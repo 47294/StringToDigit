322825a408279b85694d199418cf55f071838213
Artem <45859415+47294@users.noreply.github.com>
2021-11-06 13:29:20 +0300

    Update Check.Test.cpp

--- tests/Check.Test.cpp
+++ tests/Check.Test.cpp
@@ -9,11 +9,11 @@ TEST(Desyat, Test1)
 }
 TEST(Desyat, Test2)
 {
-	//ASSERT_DOUBLE_EQUAL();
+	ASSERT_DOUBLE_EQ(StringToDouble(".456", 10), 0.456);
 }
 TEST(Desyat, Test3)
 {
-	//ASSERT_DOUBLE_EQUAL();
+	ASSERT_DOUBLE_EQ(StringToDouble("123.", 10), 123);
 }
 TEST(Desyat, Test4)
 {
