1bbbf356476da6e154be74dbb35849cec364573e
A <A@mail.ru>
2021-11-06 13:00:10 +0300

    Begin

---
+++ src/core.c
@@ -0,0 +1,51 @@
+
+int CharToDigit(char c)
+{
+	if ('0' <= c && c <= '9')
+		return c - '0';
+	if ('A' <= c && c <= 'Z')
+		return c - 'A' + 10;
+}
+
+int StringToInt(const char* str, unsigned length, unsigned base)
+{
+	int result = 0;
+
+	for(int i = 0; i < length; ++i)
+	{
+		result *= base;
+		result += CharToDigit(str[i]);
+	}
+	return result;
+}
+unsigned SearchPoint(const char* str)
+{
+	for (int i = 0; i < strlen(str); ++i) 
+	{
+		if(str[i] == '.' || str[i] == ',') 
+			return i;
+	}
+	return strlen(str);
+}
+
+double StringToDouble(const char* str, unsigned base)
+{
+	bool isNigaLive = false;
+	if(str[0] == '-')
+	{
+		isNigaLive = true;
+		++str;
+	}
+	if(str[0] == '+')
+       	{
+		++str;
+	}
+	unsigned pointIndex = SearchPoint(str);
+	
+	//123.145
+	int fracPartLength = strlen(str) - pointIndex - 1;
+	int intPart = StringToInt(str, pointIndex, base);
+	int fracPart = StringToInt(&str[pointIndex + 1], fracPartLength, base);
+
+	return intPart + fracPart * pow(base, -fracPartLength);
+}
