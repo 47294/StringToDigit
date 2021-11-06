c4db834bec366bd606274badbae5c2c04608677e
Artem <45859415+47294@users.noreply.github.com>
2021-11-06 13:24:59 +0300

    Update Check.Test.cpp

--- tests/Check.Test.cpp
+++ tests/Check.Test.cpp
@@ -5,5 +5,22 @@ extern "C"
 }
 TEST(Desyat, Test1)
 {
+	ASSERT_DOUBLE_EQ(StringToDouble('123.456', 10), 123.456);
+}
+TEST(Desyat, Test2)
+{
 	//ASSERT_DOUBLE_EQUAL();
 }
+TEST(Desyat, Test3)
+{
+	//ASSERT_DOUBLE_EQUAL();
+}
+TEST(Desyat, Test4)
+{
+	//ASSERT_DOUBLE_EQUAL();
+}
+TEST(Desyat, Test5)
+{
+	//ASSERT_DOUBLE_EQUAL();
+}
+
